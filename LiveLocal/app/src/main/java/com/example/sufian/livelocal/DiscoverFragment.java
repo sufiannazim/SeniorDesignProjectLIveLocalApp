751d27db112f4a49ae81f19a6383b7854e56ef90
sun12002 <sufian.nazim@uconn.edu>
2015-12-02 17:46:13 -0500

    updated code for the project

---
+++ LiveLocal/app/src/main/java/com/example/sufian/livelocal/DiscoverFragment.java
@@ -0,0 +1,32 @@
+package com.example.sufian.livelocal;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DiscoverFragment extends Fragment {
+
+    TextView STTtxtView;
+
+    public DiscoverFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_discover, container, false);
+
+        return  view;
+    }
+
+}

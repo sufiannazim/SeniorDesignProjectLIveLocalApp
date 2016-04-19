7cb5e537aefc11ff5fe27c6b1e6e8f4142843216
sun12002 <sufian.nazim@uconn.edu>
2016-04-19 14:50:48 -0400

    Added missing images from the project code

---
+++ LiveLocal/app/src/main/java/com/example/sufian/livelocal/OtherWaysToLiveLocal.java
@@ -0,0 +1,73 @@
+package com.example.sufian.livelocal;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.os.Bundle;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+
+public class OtherWaysToLiveLocal extends Fragment {
+
+    private WebView mWebview;
+    private View rootView;
+
+    public OtherWaysToLiveLocal() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_other_ways_to_live_local, container, false);
+
+        WebView htmlWebView = (WebView)rootView.findViewById(R.id.webViewOtherWays);
+        htmlWebView.setWebViewClient(new CustomWebViewClient());
+        WebSettings webSetting = htmlWebView.getSettings();
+        webSetting.setJavaScriptEnabled(true);
+        webSetting.setBuiltInZoomControls(true);
+        webSetting.setDisplayZoomControls(false);
+        webSetting.setLoadWithOverviewMode(true);
+        webSetting.setUseWideViewPort(true);
+        htmlWebView.loadUrl("http://livelocal.uconn.edu/other-ways-to-live-local");
+        return rootView;
+    }
+
+    private class CustomWebViewClient extends WebViewClient{
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            rootView.findViewById(R.id.webViewOtherWays).setVisibility(View.VISIBLE);
+        }
+    }
+}
+
+
